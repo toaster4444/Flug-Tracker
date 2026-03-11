138e5d8b00ba7ce0bfda1d338e197d3d2bee75aa
toaster4444 <th.bb94@googlemail.com>
2026-03-11 20:33:06 +0100

    Create flights.js

---
+++ api/flights.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const { lamin, lomin, lamax, lomax } = req.query;
+  try {
+    const url = `https://opensky-network.org/api/states/all?lamin=${lamin}&lomin=${lomin}&lamax=${lamax}&lomax=${lomax}`;
+    const r = await fetch(url);
+    if (!r.ok) return res.status(r.status).json({ error: 'OpenSky error ' + r.status });
+    const data = await r.json();
+    res.json(data);
+  } catch(e) {
+    res.status(500).json({ error: e.message });
+  }
+}
